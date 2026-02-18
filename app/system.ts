4ab124990f7e3e13c18740821ca8c4b246c612ef
parksb <parkgds@gmail.com>
2026-02-18 21:53:02 +0900

    사이트맵 수정

--- app/system.ts
+++ app/system.ts
@@ -90,7 +90,7 @@ export class System {
       )
       .join("\n");
     return `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${urls}
       </urlset>`;
   }
